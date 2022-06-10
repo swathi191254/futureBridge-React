acf1bf74533cfa0f3fe6ee0dbc344cf515dff70a
Swathi <swathi709357@gmail.com>
2022-06-10 14:10:49 +0530

    commit

--- movie-app/src/App.jsx
+++ movie-app/src/App.jsx
@@ -1,10 +1,10 @@
 import logo from './logo.svg';
 import './App.css';
-
+import MoviePage from './components/moviePage';
 function App() {
   return (
     <div className="App">
-      
+      <MoviePage />
     </div>
   );
 }
--- movie-app/src/components/moviePage.jsx
+++ movie-app/src/components/moviePage.jsx
@@ -1,11 +1,42 @@
-import React from 'react'
+import React from "react";
+import axios from "axios";
+import "../App.css";
 
-const moviePage = () => {
+const MoviePage = () => {
+  const [movie, setMovie] = React.useState([]);
+
+  React.useEffect(() => {
+    getMovie();
+  }, []);
+
+  const getMovie = () => {
+    axios.get("http://localhost:2345/movies").then((res) => {
+      setMovie(res.data);
+    });
+  };
   return (
     <div>
-      
+      {movie.map((e) => (
+        <div className="card">
+          <div id="movie-div">
+            <h2>Name :{e.name}</h2>
+            <p>Rating :{e.rating}</p>
+            <p>Year :{e.date}</p>
+          </div>
+          <div id="movie-div">
+            <h2>Name :{e.name}</h2>
+            <p>Rating :{e.rating}</p>
+            <p>Year :{e.date}</p>
+          </div>
+          <div id="movie-div">
+            <h2>Name :{e.name}</h2>
+            <p>Rating :{e.rating}</p>
+            <p>Year :{e.date}</p>
+          </div>
+        </div>
+      ))}
     </div>
-  )
-}
+  );
+};
 
-export default moviePage
+export default MoviePage;
